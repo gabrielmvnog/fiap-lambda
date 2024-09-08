b81cde355a271c5ec6657bda9c39c27d57bd3b7c
Gabriel Nogueira <gabrielmvnogueira@gmail.com>
2024-09-08 11:41:27 -0300

    feat: add database connection

--- src/main.py
+++ src/main.py
@@ -1,15 +1,30 @@
 import os
 import sqlalchemy
+from sqlalchemy import String
+from sqlalchemy.orm import declarative_base
+from sqlalchemy import Column
+from sqlalchemy import Integer
+from sqlalchemy import select
+from sqlalchemy.orm import Session
+from sqlalchemy.exc import NoResultFound
+
+Base = declarative_base()
+
+
+class User(Base):
+    __tablename__ = "user"
+
+    id = Column(Integer, primary_key=True)
+    document_number = Column(String)
+    password = Column(String)
 
 
 def lambda_handler(event, context) -> bool:
     document_number = event["documentNumber"]
     password = event["password"]
 
-    if document_number == "documentNumber" and password == "password":
-        return True
-
-    return False
+    session = database_connection()
+    return find_user(session, document_number, password)
 
 
 def database_connection():
@@ -18,6 +33,23 @@ def database_connection():
     DB_TABLE = os.environ["DB_TABLE"]
     DB_PASS = os.environ["DB_PASS"]
 
-    return sqlalchemy.create_engine(
+    engine = sqlalchemy.create_engine(
         f"postgresql://{DB_USER}:{DB_PASS}@{DB_HOST}/{DB_TABLE}"
     )
+
+    return Session(engine)
+
+
+def find_user(session, document_number, password):
+    stmt = (
+        select(User)
+        .where(User.document_number == document_number)
+        .where(User.password == password)
+    )
+
+    try:
+        user = session.execute(stmt).scalar_one()
+    except NoResultFound:
+        return None
+
+    return user
